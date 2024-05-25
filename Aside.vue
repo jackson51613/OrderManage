fad4ae4baa56a8c799601f93ee3f6d4aba3e2222
rayrayrayr <shaonvzhonjiezhe@gmail.com>
2024-05-25 11:18:15 +0900

    Add files via upload

---
+++ Aside.vue
@@ -0,0 +1,36 @@
+<template>
+    <el-menu :default-openeds="['1']" style="min-height: 100%" background-color="rgb(48, 65, 86)" text-color="#fff"
+        :collapse-transition="false" router>
+        <div style="height: 60px;line-height: 60px;text-align: center;font-size: 18px;">
+            <img src="../assets/logo.png" alt="" style="width: 25px;position: relative;top: 8px;margin-right: 5px;">
+            <b style="color: #fff">受発注システム</b>
+        </div>
+        <el-submenu index="1">
+            <template slot="title"><i class="el-icon-menu"></i>
+                <span slot="title">暫定</span>
+            </template>
+            <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="/user">注文管理</el-menu-item>
+                <el-menu-item index="/">受注管理</el-menu-item>
+                <el-submenu index="2">
+                    <template slot="title">
+                        <span slot="title">在庫管理</span>
+                    </template>
+                    <el-menu-item index="/receivingmanagement">入庫管理一覧</el-menu-item>
+                </el-submenu>
+            </el-menu-item-group>
+
+
+        </el-submenu>
+    </el-menu>
+</template>
+
+<script>
+export default {
+    name: 'Aside',
+    props: {
+    }
+}
+
+</script>
---
+++ index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'ManageView',
+    component: () => import('../views/ManageView.vue'),
+    //重定向
+    redirect: '/user',
+    children: [
+      { path: 'user', name: "User", component: () => import('../views/User.vue'), props: true },
+      { path: 'receivingmanagement', name: "ReceivingManagement", component: () => import('../views/ReceivingManagement.vue'), props: true },
+      { 
+        path: 'neworder', 
+        name: "NewOrder", 
+        component: () => import('../views/NewOrder.vue'),
+        redirect: 'neworder/orderlist',
+        // 添加子路由
+        children: [
+          { path: 'orderlist', name: "OrderList", component: () => import('../views/ProductSelection.vue'), props: true },
+          { path: 'shippingandbilling', name: "ShippingAndBilling", component: () => import('../views/ShippingAndBilling.vue'), props: true },
+          { path: 'orderconfirmation', name: "OrderConfirmation", component: () => import('../views/OrderConfirmation.vue'), props: true },
+          
+          
+        ]
+      }
+      
+    ]
+
+    
+
+    
+  },
+]    
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
